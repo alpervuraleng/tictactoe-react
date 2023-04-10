bbcf77bb99180112f19b95ac031b55bad77c8d33
Alper Vural <alperlutfivural@gmail.com>
2023-04-10 14:02:37 -0700

    show history

--- src/App.js
+++ src/App.js
@@ -11,22 +11,23 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xTurn, setxTurn] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ handleTurn, xTurn, squares }) {
   function handleClick(i) {
-    if (squares[0]) {
+    if (squares[i] || calculateWinner()) {
       return;
     }
+    
     const nextSquares = squares.slice();
     nextSquares[i] = xTurn ? "X": "O";
-    setSquares(nextSquares);
-    setxTurn(!xTurn);
+    handleTurn(nextSquares);
   }
+
+  const winner = calculateWinner();
+  let status = winner ? `Winner: ${winner}` : "Next Player";
 
   return (
     <Fragment>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -43,5 +44,65 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </Fragment>
+  );
+
+  // Copied this function from react.dev
+  function calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xTurn, setxTurn] = useState(true);
+  const curSquares = history[history.length - 1];
+
+  function handleTurn(board) {
+    const newHistory = [...history, board];
+    setHistory(newHistory);
+    setxTurn(!xTurn);
+  }
+
+  function jumpTo(move) {
+    console.log(`Jump to ${move}`);
+  }
+
+  const moves = history.map((_, move) => {
+    const desc = `Go to ${move}`;
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board handleTurn={handleTurn} 
+               xTurn={xTurn}
+               squares={curSquares}
+        />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
